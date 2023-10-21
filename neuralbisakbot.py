744e96787dcd15f3be90de461d4b0e1fbc6b789e
Kostya Vakarian <vakarianplay@gmail.com>
2023-10-22 00:06:53 +0500

    Add files via upload

--- neuralbisakbot.py
+++ neuralbisakbot.py
@@ -20,8 +20,7 @@ text_model = markovify.Text(text)
 
 #with open('mark.txt') as f:
     #my_list = [x.strip() for x in f]
-        
-bot = telebot.TeleBot("TOKENAPI")
+bot = telebot.TeleBot("1599392796:AAFQCGtM-1DoMkJOTrMmL89YFKyXgoSQw04")
 
 #start command
 @bot.message_handler(commands=['start'])
@@ -43,18 +42,18 @@ def welcome(message):
 def inl(message):
     if message.chat.type == 'private':
         if message.text == "Рандомный нейроShit":
-            #bot.send_message(message.chat.id, random.choice(my_list));
+            #bot.send_message(message.chat.id, reply_markup=keyboard, parse_mode="Html");
             bot.send_message(message.chat.id, text_model.make_sentence())
         elif message.text == "О боте":
             bot.send_message(message.chat.id, "Бот работает на цепях Маркова и генерирует случайную фразу из военного устава, отзывов и цитат из пабликов.\nСайт автора: https://vakarian.website\nGitHub: https://github.com/vakarianplay")
-            bot.send_message(message.chat.id, 'Смотрите также:\n@youarefagotbot - бот напомнит кто ты по жизни\n@batushkin - нейроправославный канал')
+            bot.send_message(message.chat.id, 'Смотрите также:\n@goslingatorbot - мудрость от Райана Гослинга\n@cyberbibki - мой личный блог')
         else:
             bot.send_message(message.chat.id, 'Дон`т андерстенд')
 
 
 
 
-# bot.polling
+# RUN
 if __name__ == '__main__':
     while True:
         try:
