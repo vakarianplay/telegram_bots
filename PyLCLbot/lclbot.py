57b25b31f2e65d23ba5c0b41383e0660a5670019
Kostya Vakarian <vakarianplay@gmail.com>
2024-05-29 13:46:12 +0300

    Toxic_level function

--- PyLCLbot/lclbot.py
+++ PyLCLbot/lclbot.py
@@ -2,6 +2,7 @@ import telebot
 import csv
 import logging
 import random
+import time
 
 class UsersList:
     def __init__(self, filename='/home/glab/LCLbot/users.csv'):
@@ -23,11 +24,19 @@ class UsersList:
                 logging.info(f"New user joined! User id: {user_id}, Username: {username}")
 
 class BotProps:
+    toxic_dict = {
+    'комфортик дохуя': range(0, 45),
+    'токсик ебаный': range(46, 95),
+    'пиздец токсик, так еще и душнила': range(96, 146)
+    }
+    
     def __init__(self) -> None:
         pass
+        
     def fightAssets(self):
         fights = ["СЛАВЯНСКИЙ ЗАЖИМ ЯЙЦАМИ", "СЛАВЯНСКИЙ ЗАЖИМ ЛЯЖКАМИ", "расстрел ЧВК Творожок", "газовую атаку подмыхой", "прострел сундука", 
-                  "травматический ЯДЕРНЫЙ бабах", "добивание выживших", "НЕБЕСНЫЙ КАМШОТ", "АТАКУ ДРОЧЕСЛАВА"]
+                  "травматический ЯДЕРНЫЙ бабах", "добивание выживших", "НЕБЕСНЫЙ КАМШОТ", "АТАКУ ДРОЧЕСЛАВА", "установку на понос", "СЛАВЯНСКИЙ УДАР ЗАЛУПОЙ", 
+                  "АТАКУ ПОДНЕБЕСНОГО", "инцелшот", "сонярскую базу"]
         return random.choice(fights)
     
     def boobsSize(self):
@@ -38,12 +47,30 @@ class BotProps:
         mass = random.randint(50, 1488)
         return str(mass) + " грамм"
     
+    def toxicLvl(self, username):
+        lvl = random.randint(15, 146)
+        # statusToxic = self.getStatusToxic(lvl)
+        return "<b>Уровень токсичности <u>" + str(lvl) + " %</u></b>\n\n" + username + " ты <i>" + self.getToxicStatus(lvl) + "</i>"
+    
+    def getToxicStatus(self, toxicLvl):
+        for level, range in self.toxic_dict.items():
+            if toxicLvl in range:
+                return level
+    
     def fixUsername(self, message):
         if message.from_user.username == None:
             username = message.from_user.first_name
         else:
             username = message.from_user.username
         return username
+    
+    def pingProcessor(self):
+        start_time = time.time()
+        curTime = time.strftime('%H:%M:%S', time.localtime())
+        end_time = time.time()
+        execTime = end_time - start_time
+        
+        return f'Бот онлайн!\nВремя на сервере: {curTime}\nВремя выполнения: {execTime} с'
         
 
 class LCLBot:
@@ -62,8 +89,14 @@ class LCLBot:
         def handleStart(message):
             firstLine = "/help - эта справка\n\n/whoami - узнать кто я\n\n"
             secondLine = "/whois @username - узнать кто такой @username (вводить через проблел)\n\n/fight @username - устроить бой с @username (вводить через пробел)\n\n"
-            thirdLine = "/boobsmetr - узнать размер бубсов\n\n/ballsmetr - узнать массу яиц"
+            thirdLine = "/toxic - Узнать уровень токсичности @username\n\n/boobsmetr - узнать размер бубсов\n\n/ballsmetr - узнать массу яиц"
             self.bot.send_message(message.chat.id, firstLine + secondLine + thirdLine, parse_mode='html')
+            
+        @self.bot.message_handler(commands=['ping'])
+        def handleMsg(message):
+            logging.info("Ping command")
+            self.bot.reply_to(message, f"<b>PONG!</b>", parse_mode='html')
+            self.bot.reply_to(message, self.props.pingProcessor(), parse_mode='html')
             
 
         @self.bot.message_handler(commands=['whoami'])
@@ -109,11 +142,20 @@ class LCLBot:
         def handleMsg(message):
             logging.info("Balls command")
             self.bot.reply_to(message, f"<b>{message.from_user.first_name}</b>, твои яйца весят <u>{self.props.ballsMass()}!</u>", parse_mode='html')
-            
+        
+        @self.bot.message_handler(commands=['toxic'])
+        def handleMsg(message):
+            logging.info("Toxic command")
+            if len(message.text.split()) == 2:
+                username = message.text.split(' ')[1][0:]
+                toxicAns = self.props.toxicLvl(username)
+                self.bot.reply_to(message, toxicAns, parse_mode='html')
+            else:
+                self.bot.reply_to(message, "Ты долбоеб!\nПравильная команда выглядит так \n/toxic @username\nЗапоминай!")
+    
     def run(self):
         logging.info("Bot started!")
         self.bot.polling(none_stop=True)
-
 
 if __name__ == "__main__":
     TOKEN = "APITOKEN"
