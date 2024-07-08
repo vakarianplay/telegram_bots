6d0ff2d8eda70e732a4fb442a338423d2a54ec8c
Kostya Vakarian <vakarianplay@gmail.com>
2024-07-08 14:49:08 +0300

    Add files via upload

---
+++ PyLCLbot/statprocessor.py
@@ -0,0 +1,19 @@
+import csv
+import logging
+import random
+import time
+
+class StatisticCSV:
+    def __init__(self, filename='/home/kias-x/PyLCLbot/statistic.csv'):
+        self.filename = filename
+        logging.info("load statistic file")
+        
+    
+        
+def init():
+    stobj = StatisticCSV()
+
+if __name__ == "__main__":
+    print ("it is class")
+
+
